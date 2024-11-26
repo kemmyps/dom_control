6e6cecab02a9572f6c1828d64db8cc9bf28f8e7d
kemmily Souza <kemmilyps1@gmail.com>
2024-11-26 18:24:47 -0300

    Add search bar and floating action button to HomePage
    Introduced a search bar in the HomePage widget to improve content navigation. Added a floating action button with a placeholder action, featuring a red background and an add icon, to enhance user interaction.

---
+++ lib/pages/screens/home_page/aluno_list.dart
@@ -0,0 +1,95 @@
+import 'package:dom_control/pages/screens/add_aluno_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:dom_control/pages/services/firestore.dart';
+import '../aluno_details_screen.dart';
+
+class AlunoList extends StatelessWidget {
+  final FirestoreService firestoreService;
+
+  const AlunoList({super.key, required this.firestoreService});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<AlunoFirestore>>(
+      stream: firestoreService.getAlunosStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return const Center(child: Text('Error fetching data'));
+        }
+
+        final alunos = snapshot.data;
+
+        if (alunos == null || alunos.isEmpty) {
+          return const Center(child: Text('No students found'));
+        }
+
+        return ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: alunos.length,
+          itemBuilder: (context, index) {
+            final aluno = alunos[index];
+
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+              child: ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Spacer(),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          aluno.nameStudent,
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                        Text(
+                          aluno.grade,
+                          style: TextStyle(fontSize: 14.0, color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddAlunoScreen(
+                              firestoreService: firestoreService,
+                              studentId: aluno.id,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                leading: const CircleAvatar(
+                  backgroundImage: NetworkImage('https://i.pravatar.cc/30'),
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AlunoDetailsScreen(aluno: aluno),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
